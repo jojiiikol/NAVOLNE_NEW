548f9a002b137455f337882c97608bfd5ede9644
jojiiikol <133873511+jojiiikol@users.noreply.github.com>
2025-01-28 22:40:18 +0500

    Подготовка к проду

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['navolnetest.ru', '158.255.6.248', 'localhost']
+ALLOWED_HOSTS = ['navolnetest.ru', '158.255.6.248', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'django-static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 from crowdfunding import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('crow.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
