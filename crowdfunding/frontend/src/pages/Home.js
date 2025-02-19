50a7eedf35a0f83e410b0e8517534341abc77b78
lose4time <gomkabeach@gmail.com>
2025-02-19 14:07:27 +0500

    home

--- crowdfunding/frontend/src/pages/Home.js
+++ crowdfunding/frontend/src/pages/Home.js
@@ -4,34 +4,67 @@ import MyCard from '../components/cards/CardComponent';
 import '../css/home.css';
 import footage from '../images/surgut.mp4';
 import url from '../components/functions/globalURL';
+import { CSSTransition } from 'react-transition-group';
 
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
             project: null,
+            project_close: null,
+            project_new: null,
+            //showElement: false,
         };
     }
 
     componentDidMount() {
-        fetch(url + '/projects') // замените на URL вашего DRF API и ID проекта
+        //setTimeout(() => this.setState({ showElement: true }), 1000);
+
+        fetch(url + '/projects/?ordering=-views&status_code=2') // замените на URL вашего DRF API и ID проекта
             .then((response) => response.json())
             .then((data) => {
                 this.setState({ project: data.results });
-                console.log(this.state.project);
+            })
+            .catch((error) => console.error('Ошибка:', error));
+        fetch(url + '/projects/?ordering=-views&status_code=4') // замените на URL вашего DRF API и ID проекта
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({ project_close: data.results });
+                console.log(this.state.project_close);
+            })
+            .catch((error) => console.error('Ошибка:', error));
+        fetch(url + '/projects/?ordering=-start_date&status_code=2') // замените на URL вашего DRF API и ID проекта
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({ project_new: data.results });
+                console.log(this.state.project_new);
             })
             .catch((error) => console.error('Ошибка:', error));
     }
 
     render() {
-        const { project } = this.state;
+        const { project, project_close, project_new } = this.state;
+
         if (!project) {
+            return <div>Loading...</div>;
+        }
+        if (!project_close) {
+            return <div>Loading...</div>;
+        }
+        if (!project_new) {
             return <div>Loading...</div>;
         }
 
         return (
             <>
-                <section>
+                {/* <CSSTransition
+                    in={showElement}
+                    timeout={1000}
+                    classNames="fade"
+                > */}
+                {/* <section> */}
+                <div className="element">
+                    {' '}
                     <video
                         style={{
                             marginTop: '50px',
@@ -61,7 +94,10 @@ class Home extends Component {
                             привлечению капитала в ХМАО
                         </h3>
                     </div>
-                </section>
+                </div>
+
+                {/* </section> */}
+                {/* </CSSTransition> */}
 
                 <Container
                     fluid
@@ -69,35 +105,109 @@ class Home extends Component {
                         marginTop: '50px',
                     }}
                 >
-                    <div className="d-flex justify-content-between mt-4 mb-4">
-                        <h1 className="all-projects-h1">Проекты</h1>
-                        <a href="/projects" className="all-projects-href-p">
-                            Все проекты
-                        </a>
+                    <div className="mb-5">
+                        <div className="d-flex justify-content-between mt-4 mb-4">
+                            <h1 className="all-projects-h1">
+                                Популярные проекты
+                            </h1>
+                            <a href="/projects" className="all-projects-href-p">
+                                Все проекты
+                            </a>
+                        </div>
+
+                        <Row xs={1} md={3} className="g-4">
+                            {project.map((project) => (
+                                <Col>
+                                    <MyCard
+                                        key={project.pk}
+                                        slug={project.slug}
+                                        collected_money={
+                                            project.collected_money
+                                        }
+                                        need_money={project.need_money}
+                                        name={project.name}
+                                        category={project.category}
+                                        small_description={
+                                            project.small_description
+                                        }
+                                        views={project.views}
+                                        image={project.image}
+                                        code={project.status_code.code}
+                                        start_date={project.start_date}
+                                        end_date={project.end_date}
+                                    />
+                                </Col>
+                            ))}
+                        </Row>
                     </div>
 
-                    <Row xs={1} md={3} className="g-4">
-                        {project.map((project) => (
-                            <Col>
-                                <MyCard
-                                    key={project.pk}
-                                    slug={project.slug}
-                                    collected_money={project.collected_money}
-                                    need_money={project.need_money}
-                                    name={project.name}
-                                    category={project.category}
-                                    small_description={
-                                        project.small_description
-                                    }
-                                    views={project.views}
-                                    image={project.image}
-                                    code={project.status_code.code}
-                                    start_date={project.start_date}
-                                    end_date={project.end_date}
-                                />
-                            </Col>
-                        ))}
-                    </Row>
+                    <div className="mb-5">
+                        {' '}
+                        <div className="d-flex justify-content-between mt-5 mb-4">
+                            <h1 className="all-projects-h1">Новые проекты</h1>
+                            <a href="/projects" className="all-projects-href-p">
+                                Все проекты
+                            </a>
+                        </div>
+                        <Row xs={1} md={3} className="g-4">
+                            {project_new.map((project) => (
+                                <Col>
+                                    <MyCard
+                                        key={project.pk}
+                                        slug={project.slug}
+                                        collected_money={
+                                            project.collected_money
+                                        }
+                                        need_money={project.need_money}
+                                        name={project.name}
+                                        category={project.category}
+                                        small_description={
+                                            project.small_description
+                                        }
+                                        views={project.views}
+                                        image={project.image}
+                                        code={project.status_code.code}
+                                        start_date={project.start_date}
+                                        end_date={project.end_date}
+                                    />
+                                </Col>
+                            ))}
+                        </Row>
+                    </div>
+
+                    <div className="mb-5">
+                        {' '}
+                        <div className="d-flex justify-content-between mt-5 mb-4">
+                            <h1 className="all-projects-h1">Сбор окончен</h1>
+                            <a href="/projects" className="all-projects-href-p">
+                                Все проекты
+                            </a>
+                        </div>
+                        <Row xs={1} md={3} className="g-4">
+                            {project_close.map((project) => (
+                                <Col>
+                                    <MyCard
+                                        key={project.pk}
+                                        slug={project.slug}
+                                        collected_money={
+                                            project.collected_money
+                                        }
+                                        need_money={project.need_money}
+                                        name={project.name}
+                                        category={project.category}
+                                        small_description={
+                                            project.small_description
+                                        }
+                                        views={project.views}
+                                        image={project.image}
+                                        code={project.status_code.code}
+                                        start_date={project.start_date}
+                                        end_date={project.end_date}
+                                    />
+                                </Col>
+                            ))}
+                        </Row>
+                    </div>
                 </Container>
             </>
         );
